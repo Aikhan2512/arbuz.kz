f6cf6aa933dd120474622c29866e533148a44bbf
Aikhan2512 <150243233+Aikhan2512@users.noreply.github.com>
2024-03-28 17:56:42 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./App.css";
+
+export default function App() {
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1 className="header-logo">Shopify</h1>
+      </header>
+        <h1 className='glav'>Products</h1>
+        <div>
+          
+          <div>
+            <h3 className='h3'>iPhone 9</h3>
+            <p>$549</p>
+            <p>Apple</p>
+            <h4>An apple mobile which is nothing like apple</h4>
+          </div>
+        </div>
+      <footer ><p>© 2024 Shopify</p></footer>
+    </div>
+  );
+};
+
+
